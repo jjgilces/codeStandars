77935cc4260ba34c9f477a9390d571ff3aeddfe5
jjgilces <jjgilces@espol.edu.ec>
2023-05-31 23:55:38 -0500

    creating checkstyles

--- taller1/src/main/java/com/espol/edu/model/Package.java
+++ taller1/src/main/java/com/espol/edu/model/Package.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.espol.edu.model;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -12,31 +9,39 @@ import java.util.ArrayList;
  */
 public class Package {
 
-    double baseCost = 1000;
+    private static double baseCost = 1000;
+	public static Scanner sc = new Scanner(System.in);
+
     Destination destination;
-    ArrayList<String> travelers;
+    private int numberOfTravelers;
     int duration;
     double totalCost;
 
-    public Package(Destination destination, ArrayList<String> travelers) {
-        this.destination = destination;
-        if(travelers.size()>80)            
-            throw new IllegalArgumentException("Se ha excedido el límite máximo de personas por paquete.");
-        this.travelers = travelers;
-        this.totalCost += baseCost;
+   
+    public Package (Destination d, int numDays, int numTraveles) {
+    	destination=d;
+    	numberOfTravelers=numTraveles;
+    	duration= numDays;
+    	totalCost+=baseCost;
     }
-
     public double getTotalCost() {
         totalCost += destination.getCost();
-        int qTraveleres = travelers.size();
-        if (qTraveleres > 4 && qTraveleres < 10) {
+
+        if (numberOfTravelers > 4 && numberOfTravelers < 10) {
             totalCost = totalCost * 0.9;
-        } else if (qTraveleres >= 10) {
+        } else if (numberOfTravelers >= 10) {
             totalCost = totalCost * 0.8;
         }
         if(duration<7) totalCost+=200;
-        if(duration>30 && qTraveleres==2)totalCost-=200;
+        if(duration>30 && numberOfTravelers==2)totalCost-=200;
         return totalCost;
+
+    }
+    
+    public static double generateQuotePackage() {
+    	System.out.println("Welcome to ECUDREAMS:");
+    	System.out.println("To quote your vacations, please provide the follow information:");
+    	return 1;
 
     }
 }
