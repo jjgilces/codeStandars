734df29ac1c9321e5d6e206e4a48227ed0e7e1ac
Joseph Avila Alvarez <josdavilalvarez@gmail.com>
2023-06-07 18:21:04 -0500

    add requirements

--- taller1/src/main/java/com/espol/edu/model/TravelPackage.java
+++ taller1/src/main/java/com/espol/edu/model/TravelPackage.java
@@ -26,6 +26,11 @@ public final class TravelPackage {
 
     private int numberOfTravelers;
     /**
+     * Attribute is declared to define vacation package..
+     */
+
+     private int packageVacation;
+    /**
     * Attribute is declared to define the travel duration.
     */
 
@@ -53,14 +58,16 @@ public final class TravelPackage {
     /**
      * TravelPackage constructor.
      */
-    private TravelPackage(final Destination d,final int numDays,final int numTraveles){
+    private TravelPackage(final Destination d,final int numDays,final int numTraveles, int packageOption){
     	destination = d;
     	numberOfTravelers = numTraveles;
+    	
     	if (numDays > 80){
     		throw new IllegalArgumentException();
     	}
     	duration =  numDays;
     	totalCost += baseCost;
+    	packageVacation = packageOption;
     }
     /**
      * @return double
@@ -79,6 +86,14 @@ public final class TravelPackage {
         	}
         if(duration > 30 && numberOfTravelers == 2) {
         	totalCost -= 200;
+        }
+        // Add the cost of optional add-ons
+        if (packageVacation == 1) { // All-Inclusive Package
+            totalCost += numberOfTravelers * 200;
+        } else if (packageVacation == 2) { // Adventure Activities Package
+            totalCost += numberOfTravelers * 150;
+        } else if (packageVacation == 3) { // Spa and Wellness Package
+            totalCost += numberOfTravelers * 100;
         }
         return totalCost;
 
@@ -99,8 +114,14 @@ public final class TravelPackage {
     	System.out.println("Enter the number of the travelers:");
     	int numOfTravelers = sc.nextInt();
     	sc.nextLine();
+    	System.out.println("Enter the vacation package ");
+    	System.out.println("1. All-Inclusive Package - $200 per traveler");
+    	System.out.println("2. Adventure Activities Package - $150 per traveler");
+    	System.out.println("3. Spa and Wellness Package - $100 per traveler");
+    	int packageOption = sc.nextInt();
+    	sc.nextLine();
         try {
-            TravelPackage quote = new TravelPackage(destin, numOfDays, numOfTravelers);
+            TravelPackage quote = new TravelPackage(destin, numOfDays, numOfTravelers,packageOption);
             return quote.getTotalCost();
         } catch (IllegalArgumentException ex) {
             System.err.println("We could not create your package of vacations");
